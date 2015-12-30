7f5f8e5569ecd0dabc81b52ca4577bbd7bb92d68
Orvas <kelmoney@yandex.ru>
2015-12-30 18:03:22 +0500

    simple voice recording and playing it

--- GreenLightClient/src/main/java/com/globalgrupp/greenlight/greenlightclient/controller/NewEventActivity.java
+++ GreenLightClient/src/main/java/com/globalgrupp/greenlight/greenlightclient/controller/NewEventActivity.java
@@ -2,10 +2,14 @@ package com.globalgrupp.greenlight.greenlightclient.controller;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -15,6 +19,7 @@ import com.globalgrupp.greenlight.greenlightclient.classes.CreateEventOperation;
 import com.globalgrupp.greenlight.greenlightclient.classes.CreateEventParams;
 import com.globalgrupp.greenlight.greenlightclient.classes.SimpleGeoCoords;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
@@ -27,6 +32,16 @@ public class NewEventActivity extends ActionBarActivity implements AdapterView.O
 
     private SimpleGeoCoords eLocation;
     private Address eAddres;
+
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+    private static String mFileName = null;
+    private static final String LOG_TAG = "AudioRecordTest";
+
+    Button btnAudio;
+    Button btnPlayAudio;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,7 +67,10 @@ public class NewEventActivity extends ActionBarActivity implements AdapterView.O
                 }
             });
             setSupportActionBar(mActionBarToolbar);
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/audiorecordtest.3gp";
             setEvents();
+            btnPlayAudio.setVisibility(View.INVISIBLE);
         }
         catch (Exception e){
             e.printStackTrace();
@@ -69,6 +87,8 @@ public class NewEventActivity extends ActionBarActivity implements AdapterView.O
         super.onBackPressed();
     }
 
+    boolean mStartRecording = true;
+    boolean mStartPlaying = true;
     public void setEvents(){
         final Button createEventButton= (Button) findViewById(R.id.btnCreateEvent);
         createEventButton.setOnClickListener(new View.OnClickListener() {
@@ -86,5 +106,104 @@ public class NewEventActivity extends ActionBarActivity implements AdapterView.O
             }
         });
 
+        btnAudio=(Button)findViewById(R.id.btnAudio);
+        btnAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    onRecord(mStartRecording);
+                    if (mStartRecording) {
+                        btnAudio.setText("Стоп");
+                    } else {
+
+                        btnAudio.setText("Аудио");
+                    }
+                    mStartRecording = !mStartRecording;
+                }catch (Exception e){
+                    Log.e("Audio error",e.getMessage());
+                }
+            }
+        });
+        btnPlayAudio=(Button)findViewById(R.id.btnPlayAudio);
+        btnPlayAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    btnPlayAudio.setText("Стоп");
+                } else {
+                    btnPlayAudio.setText("Воспроизвести");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        });
+
+    }
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+            btnPlayAudio.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 }
